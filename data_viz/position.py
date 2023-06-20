9f062b55664e2492dcc467bdec283e2ccfe4405b
drupman <tomasgaudino@hotmail.com>
2023-06-19 22:43:17 -0300

    (feat) add positions

--- data_viz/position.py
+++ data_viz/position.py
@@ -24,14 +24,6 @@ class PositionsExecuted:
                                      close=self.candles["close"]),
                       col=1,
                       row=1)
-        fig.add_trace(go.Scatter(name="Positions",
-                                 x=self.df["datetime"],
-                                 y=self.df["entry_price"],
-                                 mode="markers",
-                                 marker={"color": "yellow"},
-                                 ),
-                      col=1,
-                      row=1)
         fig.add_trace(go.Scatter(name="PnL (BUSD)",
                                  x=self.df["datetime"],
                                  y=self.df["net_pnl_quote"].cumsum(),
@@ -42,6 +34,7 @@ class PositionsExecuted:
                       row=2)
         fig.update_layout(xaxis_rangeslider_visible=False,
                           height=900)
+        fig = self.add_positions(fig)
         return fig
 
     def get_close_type_by_side(self):
@@ -57,3 +50,71 @@ class PositionsExecuted:
                       y="side",
                       color="close_type",
                       orientation="h")
+
+    def add_positions(self, base_figure):
+        self.df.loc[self.df['side_num'] == -1, 'fig'] = 'triangle-down'
+        self.df.loc[self.df['side_num'] == 1, 'fig'] = 'triangle-up'
+        self.df.loc[self.df['real_class'] == 1, 'color'] = 'lightgreen'
+        self.df.loc[self.df['real_class'] == -1, 'color'] = 'red'
+        base_figure.add_trace(go.Scatter(x=self.df.loc[(self.df['side'] != 0), 'datetime'],
+                                         y=self.df.loc[self.df['side'] != 0, 'entry_price'],
+                                         name='Entry Price: $',
+                                         mode='markers',
+                                         marker_color=self.df.loc[(self.df['side'] != 0), 'color'],
+                                         marker_symbol=self.df.loc[(self.df['side'] != 0), 'fig'],
+                                         marker_size=20,
+                                         marker_line={'color': 'black', 'width': 0.7}))
+
+        for index, row in self.df.iterrows():
+            tp = row.tp * self.tp_viz_factor
+            base_figure.add_shape(type="rect",
+                                  fillcolor="green",
+                                  opacity=0.5,
+                                  x0=row.datetime,
+                                  y0=row.entry_price,
+                                  x1=row.close_datetime,
+                                  y1=row.entry_price * (1 + (tp) * row.side_num),
+                                  line=dict(color="green"))
+            # Add SL
+            base_figure.add_shape(type="rect",
+                                  fillcolor="red",
+                                  opacity=0.5,
+                                  x0=row.datetime,
+                                  y0=row.entry_price,
+                                  x1=row.close_datetime,
+                                  y1=row.entry_price * (1 - row.sl * row.side_num),
+                                  line=dict(color="red"))
+        return base_figure
+
+    def add_macd(self, slow=12, fast=26, signal=9):
+        df = self.candles.copy()
+        # df.ta.macd(slow=slow, fast=fast, signal=signal, append=True)
+        # Add MACD with signal to lower subplot
+        self.base_figure.add_trace(go.Scatter(x=df['timestamp'],
+                                              y=df[f'MACD_{slow}_{fast}_{signal}'],
+                                              name="MACD",
+                                              marker_color="blue"),
+                                   row=3,
+                                   col=1)
+
+        self.base_figure.add_trace(go.Scatter(x=df['timestamp'],
+                                              y=df[f'MACDs_{slow}_{fast}_{signal}'],
+                                              name="Signal",
+                                              marker_color="red"),
+                                   row=3,
+                                   col=1)
+
+        # Add the MACD histogram to the lower subplot
+        self.base_figure.add_trace(go.Bar(x=df['timestamp'],
+                                          y=df[f'MACDh_{slow}_{fast}_{signal}_norm'],
+                                          marker=dict(color=df[f'MACDh_{slow}_{fast}_{signal}_norm'].apply(lambda x: 'lightgreen' if x >= 0 else 'red')),
+                                          name="MACD Hist"),
+                                   row=3,
+                                   col=1)
+
+        self.base_figure.add_trace(go.Bar(x=df['timestamp'],
+                                          y=df[f'cum_macd_filtered'],
+                                          marker_color='aqua',
+                                          name="Delta MACD"),
+                                   row=3,
+                                   col=1)
